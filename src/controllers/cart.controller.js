9f0e5e5cf7232b4e99eeb0a8aac6aa593bf77534
9r3dflam3 <167046779+9r3dflam3@users.noreply.github.com>
2024-06-18 20:27:58 +0900

    chore : Cart Message Add

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,10 +1,43 @@
 import { HTTP_STATUS } from '../constants/http-status.constant.js';
 import { MESSAGES } from '../constants/message.constant.js';
 
+/* nessesary Prisma Table
+: users
+  - id
+  - address
+
+: stores
+  - id
+
+: menus
+  - id
+
+*/
+
 export class CartController {
   constructor(cartService) {
     this.cartService = cartService;
   }
 
   // method 작성하시면 됩니다.
+
+  /*
+   carts Create API
+
+   req.header : id (users)
+
+   req.body : 
+    store_id ( 내가 선택한 가게가 default로 입력? )
+    order { menu_id , address(default : users - address) }
+  
+   */
+
+  createCart = async (req, res, next) => {
+    try {
+      const { costomerId } = req.user;
+      const { storeId } = req.header;
+    } catch {
+      next();
+    }
+  };
 }
