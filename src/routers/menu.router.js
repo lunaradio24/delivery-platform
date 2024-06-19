4fca7c9a9bde47c0d7c1641552a17ddfc9d1a115
blueclorox <dehydrate92@gmail.com>
2024-06-20 00:24:38 +0900

    chore/ menu
    오타 수정

--- src/routers/menu.router.js
+++ src/routers/menu.router.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import { menuController } from '../di/dependency-injected-instances.js';
 import { requireAccessToken } from '../middlewares/require-access-token.middleware.js';
+import { requireRoles } from '../middlewares/require-roles.middleware.js';
 
 const menuRouter = express.Router({ mergeParams : true });
 
 // 메뉴 생성 API
-menuRouter.post('/', requireAccessToken, menuController.createMenu)
+menuRouter.post('/', requireAccessToken, requireRoles(['OWNER']), menuController.createMenu)
 
 // 메뉴 목록 조회 API
 menuRouter.get('/', menuController.getMenu)
 
 // 메뉴 수정 API
-menuRouter.patch('/:menuId', requireAccessToken, menuController.updateMenu)
+menuRouter.patch('/:menuId', requireAccessToken, requireRoles(['OWNER']), menuController.updateMenu)
 
 // 메뉴 삭제 API
-menuRouter.delete('/:menuId', requireAccessToken, menuController.deleteMenu)
+menuRouter.delete('/:menuId', requireAccessToken, requireRoles(['OWNER']), menuController.deleteMenu)
 
 export { menuRouter };
