f8dd16fe5cfbcdc8f91c926130df8504d6e2c364
lunaradio24 <91360383+lunaradio24@users.noreply.github.com>
2024-06-19 21:53:43 +0900

    Update order.router.js

--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -1,29 +1,23 @@
 import express from 'express';
-import { prisma } from '../utils/prisma.util.js';
-import { OrderRepository } from '../repositories/order.repository.js';
-import { OrderService } from '../services/order.service.js';
-import { OrderController } from '../controllers/order.controller.js';
+import { orderController } from '../di/dependency-injected-instances.js';
 import { requireRoles } from '../middlewares/requirer-roles.middleware.js';
-import { createOrderValidator } from '../middlewares/validators/create-order-validator.middleware.js';
+import { createOrderValidator } from '../middlewares/validators/create-order-validator.middleware.js';'
+  
 const orderRouter = express.Router();
 
-const orderRepository = new OrderRepository(prisma);
-const orderService = new OrderService(orderRepository);
-const orderController = new OrderController(orderService);
-
 // 주문 요청 API
-orderRouter.post('/orders', createOrderValidator, orderController.createOrder); // create joi 미들웨어 필요
+orderRouter.post('/', createOrderValidator, orderController.createOrder); // create joi 미들웨어 필요
 
 // 주문 취소 API
-orderRouter.patch('/orders/:orderId', orderController.cancelOrder); // update joi 미들웨어 필요
+orderRouter.patch('/:orderId', orderController.cancelOrder); // update joi 미들웨어 필요
 
 // 주문 내역 목록 조회 API
-orderRouter.get('/orders', orderController.getOrders);
+orderRouter.get('/', orderController.getOrders);
 
 // 주문 내역 상세 조회 API
-orderRouter.get('/orders/:orderId', orderController.getDetailOrder);
+orderRouter.get('/:orderId', orderController.getDetailOrder);
 
 // 주문 상태 변경 API
-orderRouter.patch('/orders/:orderId/status', requireRoles(['OWNER']), orderController.statusUpdateOrder); //update joi 미들웨어 필요
+orderRouter.patch('/:orderId/status', requireRoles(['OWNER']), orderController.statusUpdateOrder); //update joi 미들웨어 필요
 
 export { orderRouter };
